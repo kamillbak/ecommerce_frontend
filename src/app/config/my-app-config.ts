c2c4652ad3d6873fa4806242272956121c0a2bfe
kamillbak <kamil1427@o2.pl>
2023-07-22 17:07:01 +0200

    Login/logout files - not workin, need to use httpS

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  oidc: {
+      clientId: 'xOpQt6C75q6qEBpwOWez5riQIYn5URQO',
+      issuer: 'https:dev-wl2227di3ot1hezi.us.auth0.com/oauth2/default',
+      redirectUri: 'http://localhost:4200/login/callback',
+      scopes: ['openid', 'profile', 'email']
+  }
+
+}
